6c0f5e2e2d3c35677ea4889dd153791c5c62dc9f
Dekken <philip.deegan@gmail.com>
2016-08-12 12:44:58 +0100

    Update tcp.base.hpp

--- inc/kul/tcp.base.hpp
+++ inc/kul/tcp.base.hpp
@@ -47,7 +47,7 @@ class ASocket{
 		virtual bool connect(const std::string& host, const int16_t& port)  = 0;
 		virtual bool close()    = 0;
 		virtual uint16_t read(T* data, const uint16_t& len) throw(kul::tcp::Exception) = 0;
-		virtual bool write(const T* data, const uint16_t& len) = 0;
+		virtual int16_t write(const T* data, const uint16_t& len) = 0;
 
 };
 
