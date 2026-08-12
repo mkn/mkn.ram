eb257a13d5166cf606a3e5fce30e24aeef700957
PhilipDeegan <philip.deegan@gmail.com>
2026-08-12 19:41:26 +0200

    ++

--- inc/mkn/ram/http.hpp
+++ inc/mkn/ram/http.hpp
@@ -36,8 +36,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define _MKN_RAM_HTTP_HPP_
 
 #include "mkn/kul/map.hpp"
-#include "mkn/kul/string.hpp"
 #include "mkn/ram/tcp.hpp"
+#include "mkn/kul/string.hpp"
+
+#include <functional>
 
 namespace mkn {
 namespace ram {
